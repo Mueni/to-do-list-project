7ca0491af05cd91603402ff97113ba73a1217172
jtaraya <jacobtaraya@gmail.com>
2022-02-25 00:10:31 +0300

    paragraph.classList.add('paragraph-styling'

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let inputField = document.getElementById('inputField');
 
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling')
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
 })
